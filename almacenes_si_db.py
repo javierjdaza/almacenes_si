f987732cfe85fd968b4f9918c90fb2451483138d
Javier Javier Daza <javierjdaza@gmail.com>
2024-07-13 16:11:25 -0500

    Add files via upload

--- almacenes_si_db.py
+++ almacenes_si_db.py
@@ -5,6 +5,35 @@ import mysql.connector
 from datetime import datetime
 
 
+def forecast_key_format(row):
+    familias_tipo_1 = [278,212,213,225,276,238,270,206,260,261,262,263,275,205,257,258,259] # formato = [cod_fami]
+    familias_tipo_2 = [244,245,246,247,202,269,230,252,277,201,264,265,266,267,] # formato = [cod_fami,cod_subg]
+    familias_tipo_3 = [239,240,243,] # formato = [cod_fami, cod_subg, cod_dsub]
+    familias_tipo_4 = [241, 242, 209] # formato = [cod_fami, cod_grup, cod_subg]
+    familias_tipo_5 = [219, 214] # formato = [cod_fami, cod_tipo, cod_subg]
+    familias_tipo_6 = [250] # formato = [cod_fami, cod_tipo, cod_grup, cod_subg]
+    familias_tipo_7 = [280,251,281,268] # formato = [cod_fami, cod_tipo, cod_grup, cod_subg,cod_marca]
+    familias_tipo_8 = [248] # formato = [cod_fami, cod_subg, cod_dsub, cod_tlla]
+    familias_tipo_9 = [211] # formato = [cod_fami,cod_grup]
+
+    if int(row['cod_fami']) in familias_tipo_1:
+        return row['cod_fami']
+    elif int(row['cod_fami']) in familias_tipo_2:
+        return row['cod_fami'] + row['cod_subg'] 
+    elif int(row['cod_fami']) in familias_tipo_3:
+        return row['cod_fami'] + row['cod_subg'] + row['cod_dsub']
+    elif int(row['cod_fami']) in familias_tipo_4:
+        return row['cod_fami'] + row['cod_grup'] + row['cod_subg']
+    elif int(row['cod_fami']) in familias_tipo_5:
+        return row['cod_fami'] + row['cod_tipo'] + row['cod_subg']
+    elif int(row['cod_fami']) in familias_tipo_6:
+        return row['cod_fami'] + row['cod_tipo'] + row['cod_grup'] + row['cod_subg']
+    elif int(row['cod_fami']) in familias_tipo_7:
+        return row['cod_fami'] + row['cod_tipo'] + row['cod_grup'] + row['cod_subg'] + row['cod_marc']
+    elif int(row['cod_fami']) in familias_tipo_8:
+        return row['cod_fami'] + row['cod_subg'] + row['cod_dsub'] + row['cod_tlla']
+    elif int(row['cod_fami']) in familias_tipo_9:
+        return row['cod_fami'] + row['cod_grup']    
 
 
 def query_all_table_from_mysql(table_name,db_config):
@@ -44,26 +73,37 @@ def edited_query_from_table_mysql(db_config,sql_query):
 
 def get_sales_last_year():
     load_dotenv()
-
-    #Configurando Datos de Conexión a la DB
-    db_config = {
-        "host": os.getenv("MYSQL_HOST"),
-        "user": os.getenv("MYSQL_USER"),
-        "password": os.getenv("MYSQL_PASSWORD"),
-        "database": os.getenv("MYSQL_DATABASE")
-    }
-
-    df_product_raw = query_all_table_from_mysql("product_information",db_config)
-
     today = datetime.now()
     year_now = today.year
     last_year = year_now - 1
     min_date = f'{last_year}-01-01'
     max_date = f'{last_year}-12-31'
-    df_ventas = edited_query_from_table_mysql(db_config,f'SELECT * FROM sales where `Date` between "{min_date}" and "{max_date}"')
+    # check if the last year sales file exist, if not, create
+    if not os.path.exists(f'./datasets/historico_ventas_{last_year}_semanal.csv'):
 
-    df_product_raw = df_product_raw[['ProductId','cod_fami']]
-    df_last_year_sales = df_ventas.merge(df_product_raw, how = 'left', on = 'ProductId')
+        #Configurando Datos de Conexión a la DB
+        db_config = {
+            "host": os.getenv("MYSQL_HOST"),
+            "user": os.getenv("MYSQL_USER"),
+            "password": os.getenv("MYSQL_PASSWORD"),
+            "database": os.getenv("MYSQL_DATABASE")
+        }
 
-    return df_last_year_sales
+        df_product_raw = query_all_table_from_mysql("product_information",db_config)
+        df_product_raw['combination'] = df_product_raw.apply(lambda x: forecast_key_format(x), axis = 1) # calculate forecastkey (combination)
+
+        
+        df_ventas = edited_query_from_table_mysql(db_config,f'SELECT * FROM sales where `Date` between "{min_date}" and "{max_date}"')
+
+        df_product_raw = df_product_raw[['ProductId','cod_fami','combination']]
+        df_last_year_sales = df_ventas.merge(df_product_raw, how = 'left', on = 'ProductId')
+        df_last_year_sales['Date'] = pd.to_datetime(df_last_year_sales['Date'])
+        # Group By Week
+        df_last_year_sales_by_week = df_last_year_sales.groupby(['combination', pd.Grouper(key='Date', freq='W-MON')]).agg({'Quantity': 'sum', 'StoreId':'max','ProductId':'max'}).reset_index() 
+        df_last_year_sales_by_week.rename(columns = {'Quantity': 'quantity', 'StoreId':'store_id','ProductId':'product_id', 'Date':'date'}, inplace = True)
+        df_last_year_sales_by_week.to_csv(f'./datasets/historico_ventas_{last_year}_semanal.csv', sep = ',', index = False)
+    else:
+        df_last_year_sales_by_week = pd.read_csv(f'./datasets/historico_ventas_{last_year}_semanal.csv')
+
+    return df_last_year_sales_by_week
 
